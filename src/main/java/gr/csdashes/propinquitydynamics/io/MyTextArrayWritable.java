0f1c3cced4161d7d455e8c39accd6c6432edb5a6
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-07-12 18:39:05 +0200

    accept collections

--- src/main/java/gr/csdashes/propinquitydynamics/io/MyTextArrayWritable.java
+++ src/main/java/gr/csdashes/propinquitydynamics/io/MyTextArrayWritable.java
@@ -1,5 +1,6 @@
 package gr.csdashes.propinquitydynamics.io;
 
+import java.util.Collection;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 
@@ -34,6 +35,30 @@ public class MyTextArrayWritable extends ArrayWritable {
         for (String v : values) {
             if (!v.equals(skipString)) {
                 // The i++ first returns and then increases
+                textArray[i++] = new Text(v);
+            }
+        }
+
+        this.set(textArray);
+    }
+    
+    public MyTextArrayWritable(Collection<String> values) {
+        super(Text.class);
+        Text[] textArray = new Text[values.size()];
+        int i = 0;
+        for (String v : values) {
+            textArray[i++] = new Text(v);
+        }
+
+        this.set(textArray);
+    }
+    
+    public MyTextArrayWritable(Collection<String> values, String skipString) {
+        super(Text.class);
+        Text[] textArray = new Text[values.size()-1];
+        int i = 0;
+        for (String v : values) {
+            if (!v.equals(skipString)) {
                 textArray[i++] = new Text(v);
             }
         }
