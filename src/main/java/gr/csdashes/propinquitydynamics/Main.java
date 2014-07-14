8d7741e119fba20628d59d8d817113f08cc295e5
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-07-14 03:25:59 +0200

    extra arguments

--- src/main/java/gr/csdashes/propinquitydynamics/Main.java
+++ src/main/java/gr/csdashes/propinquitydynamics/Main.java
@@ -22,7 +22,7 @@ import org.apache.hama.graph.GraphJob;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
-        if (args.length != 2) {
+        if (args.length < 4) {
             printUsage();
         }
         HamaConfiguration conf = new HamaConfiguration(new Configuration());
@@ -33,22 +33,31 @@ public class Main {
                     + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
         }
     }
-    
+
     private static void printUsage() {
-        System.out.println("Usage: <input> <output>");
+        System.out.println("Usage: <a> <b> <input> <output> <optional:tasks>");
         System.exit(-1);
     }
 
     private static GraphJob createJob(String[] args, HamaConfiguration conf) throws IOException {
 //        conf.set("hama.graph.vertices.info", "org.apache.hama.graph.InMemoryVerticesInfo");
-        
+
+        conf.setInt("a", Integer.parseInt(args[0]));
+        conf.setInt("b", Integer.parseInt(args[1]));
+
         GraphJob graphJob = new GraphJob(conf, Main.class);
         graphJob.setJobName("Propinquity Dynamics");
         graphJob.setVertexClass(PDVertex.class);
-        graphJob.setJar("target/PropinquityDynamics-1.0-SNAPSHOT.jar");
+        graphJob.setJar("PropinquityDynamics-1.0-SNAPSHOT.jar");
 
-        graphJob.setInputPath(new Path(args[0]));
-        graphJob.setOutputPath(new Path(args[1]));
+        if (!args[4].isEmpty()) {
+            graphJob.setNumBspTask(Integer.parseInt(args[4]));
+        } else {
+            graphJob.setNumBspTask(10);
+        }
+
+        graphJob.setInputPath(new Path(args[2]));
+        graphJob.setOutputPath(new Path(args[3]));
 
         graphJob.setVertexIDClass(Text.class);
         graphJob.setVertexValueClass(MapWritable.class);
